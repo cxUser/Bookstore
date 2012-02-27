8ae6eb5c062a7c2616e70d5cb1eb39f7ac5b377d
unknown <marian@LENOVO-17D3207E.dm.cx>
2012-02-27 13:22:04 +0200

    .

---
+++ BookStore.NET/ClassLibrary1/Copy of 22.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace ConsoleApplication1
+{
+
+
+   class Program
+    {
+        static void Main(string[] args)
+        {
+	string password = "login";
+	Login(password);
+            int c = 2;
+            int d=3;	 
+            if (c == 2) 
+                Console.Write("bbbb");
+            foo();
+
+        }
+        static int foo()
+        {
+            return 1;
+        }
+
+    }
+}
---
+++ BookStore.NET/ClassLibrary1/Copy of Copy of 11.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace ConsoleApplication1
+{
+
+
+   class Program
+    {
+        static void Main(string[] args)
+        {
+	string password = "login";
+	string masha = "ff";
+	Login(password);
+    Login(masha);
+            int c = 2;
+            int d=3;	 
+            if (c == 2) 
+                Console.Write("bbbb");
+            foo();
+
+        }
+        static int foo()
+        {
+            return 1;
+        }
+
+    }
+}
